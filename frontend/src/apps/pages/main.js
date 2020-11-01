c0333d0704219fb529b8a0b565b13a467e992611
sandreae <git@contact.samandreae.com>
2020-11-01 17:53:35 +0000

    Install vue-axe for accessibility tests

--- frontend/src/apps/pages/main.js
+++ frontend/src/apps/pages/main.js
@@ -4,6 +4,11 @@ import router from '../../router'
 
 Vue.config.productionTip = false
 
+if (process.env.NODE_ENV === 'development') {
+  const VueAxe = require('vue-axe').default
+  Vue.use(VueAxe)
+}
+
 new Vue({
   router,
   render: function (h) { return h(App) }
