fb526741d5a512bfe9234e444fef073e3af97c31
sandreae <git@contact.samandreae.com>
2020-11-06 16:39:36 +0000

    Order pages before sending to frontend

--- server/pages/views.py
+++ server/pages/views.py
@@ -14,6 +14,7 @@ def get_pages():
             'body': page.body,
             'nav_position': page.nav_position
           })
+    pages.sort(key=lambda x: x.get('nav_position'))
     return pages
 
 
