ec219535878e07f1066b3bfbd39085f26950b750
sandreae <git@contact.samandreae.com>
2020-11-15 00:23:43 +0000

    Authenticate user logged in on API routes

--- server/dashboard/rpc_methods.py
+++ server/dashboard/rpc_methods.py
@@ -1,40 +1,51 @@
 from modernrpc.core import rpc_method
-from django.contrib.auth.decorators import login_required
+from modernrpc.auth.basic import http_basic_auth_login_required
 from fabric import Connection
+from invoke import Responder
 import os
 
 host = os.getenv('JACKTRIP_SERVER_ADDRESS')
 username = os.getenv('JACKTRIP_SERVER_USER')
 password = os.getenv('JACKTRIP_SERVER_PASSWORD')
 
-c = Connection(host, username, connect_kwargs={
-        "password": password,
-    },)
+sudopass = Responder(
+    pattern=r'\[sudo\] password:',
+    response=password + '\n',
+)
+
+c = Connection(host, username, connect_kwargs={"password": password,})
 
 
-@login_required
 @rpc_method
+@http_basic_auth_login_required
 def get_fpp():
     result = c.run('cat /etc/jacktrip_pypatcher/jackd.conf')
     return result.stdout.strip()
 
 
-@login_required
+@http_basic_auth_login_required
 @rpc_method
 def get_q():
     result = c.run('cat /etc/jacktrip_pypatcher/jacktrip.conf')
     return result.stdout.strip()
 
 
-@login_required
+@http_basic_auth_login_required
 @rpc_method
 def set_fpp(fpp_value):
     result = c.run('echo "FPP=%s" > /etc/jacktrip_pypatcher/jackd.conf' % (fpp_value))
     return result.exited
 
 
-@login_required
+@http_basic_auth_login_required
 @rpc_method
 def set_q(q_value):
     result = c.run('echo "Q=%s" > /etc/jacktrip_pypatcher/jacktrip.conf' % (q_value))
     return result.exited
+
+
+@http_basic_auth_login_required
+@rpc_method
+def restart_jacktrip():
+    result = c.run('sudo whoami', pty=True, watchers=[sudopass])
+    return result.stdout.strip()
