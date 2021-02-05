393d4888bb6949d6cd470e518b7e74e9d298ee7d
sandreae <git@contact.samandreae.com>
2021-02-05 12:34:06 +0000

    Boost python to v3.8

--- server/dashboard/rpc_methods.py
+++ server/dashboard/rpc_methods.py
@@ -3,6 +3,8 @@ from modernrpc.auth.basic import http_basic_auth_login_required
 from fabric import Connection
 from invoke import Responder
 from environs import Env
+from linode_api4 import LinodeClient
+import schedule
 
 env = Env()
 env.read_env()
@@ -10,13 +12,22 @@ env.read_env()
 host = env("JACKTRIP_SERVER_ADDRESS", default="123.123.123.123")
 username = env("JACKTRIP_SERVER_USER", default="user")
 password = env("JACKTRIP_SERVER_PASSWORD", default="password")
+token = env("LINODE_PAT", default="")
 
 sudopass = Responder(
     pattern=r'\[sudo\] password for fabric:',
     response=password + '\n',
 )
 
-c = Connection(host, username, connect_timeout=4, connect_kwargs={"password": password})
+f = open("/root/.ssh/id_rsa.pub", "r")
+pkey = f.read().rstrip('\n')
+f.close()
+
+client = LinodeClient('{}'.format(token))
+c = Connection(host, username, connect_timeout=4, connect_kwargs={
+        "key_filename": "/root/.ssh/id_rsa",
+    },
+)
 
 @rpc_method
 @http_basic_auth_login_required
@@ -80,3 +91,66 @@ def restart_jackd():
     """
     result = c.run('sudo systemctl restart jackd.service', pty=True, watchers=[sudopass])
     return result.exited
+
+
+@http_basic_auth_login_required
+@rpc_method
+def create_server():
+    """
+    Create a py_patcher server on Linode.
+    """
+
+    # Nanode for safety, while developing
+    type_id = "g6-nanode-1"
+    # type_id = "g6-dedicated-8"
+    # curl https://api.linode.com/v4/linode/types | jq '."data"[] | ."label"'
+
+    # London
+    region_id = "eu-west"
+    image = "linode/ubuntu18.04"
+
+    linode, password = client.linode.instance_create(type_id, region_id,
+                            image=image, authorized_keys=pkey)
+
+    if not linode:
+        raise RuntimeError("it didn't work")
+
+    return {"ip": linode.ipv4[0]}
+
+
+@http_basic_auth_login_required
+@rpc_method
+def fetch_all_servers():
+    """
+    Fetch all linode server instances
+    """
+    my_linodes = client.linode.instances()
+
+    for current_linode in my_linodes:
+        print(current_linode.label)
+
+    return my_linodes[0].label
+
+
+@http_basic_auth_login_required
+@rpc_method
+def delete_all_servers():
+    """
+    Fetch all linode server instances
+    """
+    my_linodes = client.linode.instances()
+
+    for current_linode in my_linodes:
+        print("delete: ", current_linode.label)
+        current_linode.delete()
+
+    return "success"
+
+
+@http_basic_auth_login_required
+@rpc_method
+def public_key():
+    f = open("/root/.ssh/id_rsa.pub", "r")
+    file = f.read().rstrip('\n')
+    f.close()
+    return file
