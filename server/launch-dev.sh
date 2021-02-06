b719e91e91675dfdeca366b59d8ebad59ba28807
sandreae <git@contact.samandreae.com>
2021-02-06 19:59:18 +0000

    Enable gunicorn threaded workers

--- server/launch-dev.sh
+++ server/launch-dev.sh
@@ -2,4 +2,4 @@
 
 yes | ssh-keygen -f /root/.ssh/id_rsa -t rsa -N '' &&
 python manage.py collectstatic --noinput && 
-gunicorn config.wsgi -b 0.0.0.0:8000
+gunicorn config.wsgi -b 0.0.0.0:8000 --workers=3 --threads 2
