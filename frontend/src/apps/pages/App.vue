e9138c291668317bc399297f441ff5d6b9950070
sandreae <git@contact.samandreae.com>
2021-10-05 14:56:20 +0100

    Make linter happy App.js

--- frontend/src/apps/pages/App.vue
+++ frontend/src/apps/pages/App.vue
@@ -3,8 +3,12 @@
     <P5jsBackground></P5jsBackground>
 
     <div class="container">
-      <nav class="desktop-nav nav" aria-label="desktop-nav-bar"><NavBar :routes="data.pages"></NavBar></nav>
-      <nav class="mobile-nav nav" aria-label="mobile-nav-bar"><NavBarMobile :routes="data.pages"></NavBarMobile></nav>
+      <nav class="desktop-nav nav" aria-label="desktop-nav-bar">
+        <NavBar :routes="data.pages"></NavBar>
+      </nav>
+      <nav class="mobile-nav nav" aria-label="mobile-nav-bar">
+        <NavBarMobile :routes="data.pages"></NavBarMobile>
+      </nav>
       <main class="main">
         <div class="main__padding"></div>
         <router-view class="main__content"></router-view>
@@ -17,40 +21,40 @@
 
 <script>
 // @ is an alias to /src
-import NavBar from '@/components/NavBar.vue'
-import NavBarMobile from '@/components/NavBarMobile.vue'
-import Footer from '@/components/Footer.vue'
-import P5jsBackground from '@/components/P5jsBackground.vue'
-import PageView from '../../views/page.vue'
-import Home from '@/components/Home.vue'
-import Listen from '@/components/Listen.vue'
-import P5 from 'p5'
+import NavBar from '@/components/NavBar.vue';
+import NavBarMobile from '@/components/NavBarMobile.vue';
+import Footer from '@/components/Footer.vue';
+import P5jsBackground from '@/components/P5jsBackground.vue';
+import PageView from '../../views/page.vue';
+import Home from '@/components/Home.vue';
+import Listen from '@/components/Listen.vue';
 
 export default {
   name: 'App',
   // data: function() {
   //   return {pages: {}}
   // },
-  data: function() {
+  data: function () {
     return {
       data: {
-        slug: "about",
-        pages: [{
-          slug: "about",
-          title: "About",
-          body: "About stuff",
-          nav_position: "02",
-          nav_parents: "none",
-        }]
-      }
-    }
+        slug: 'about',
+        pages: [
+          {
+            slug: 'about',
+            title: 'About',
+            body: 'About stuff',
+            nav_position: '02',
+            nav_parents: 'none',
+          },
+        ],
+      },
+    };
   },
   components: {
     NavBar,
     NavBarMobile,
     P5jsBackground,
     Footer,
-    Home
   },
   computed: {
     routes() {
@@ -59,82 +63,83 @@ export default {
   },
   methods: {
     generate_route: function (page, component) {
-      let new_route = {
+      const newRoute = {
         path: `/${page.slug}`,
         name: page.title,
         component: component,
-        props: {page: page}
-      }
-      this.$router.options.routes.push(new_route)
-      this.$router.addRoutes([new_route])
+        props: { page: page },
+      };
+      this.$router.options.routes.push(newRoute);
+      this.$router.addRoutes([newRoute]);
     },
     generate_page_routes: function () {
       for (const page of this.data.pages) {
-        if (page.slug === "home") {
-          this.generate_route(page, Home)
-          continue
+        if (page.slug === 'home') {
+          this.generate_route(page, Home);
+          continue;
         }
-        if (page.slug === "listen") {
-          this.generate_route(page, Listen)
-          continue
+        if (page.slug === 'listen') {
+          this.generate_route(page, Listen);
+          continue;
         }
-        this.generate_route(page, PageView)
+        this.generate_route(page, PageView);
       }
     },
-    create_default_pages(){
-      let listen = {
-        slug: "listen",
-        title: "Listen",
-        body: "",
-        nav_position: "02",
-        nav_parents: "none",
-      }
-      let home = {
-        slug: "home",
-        title: "Home",
-        body: "",
-        nav_position: "01",
-        nav_parents: "none",
-      }
-      this.data.pages.push(listen)
-      this.data.pages.push(home)
-    }
+    create_default_pages() {
+      const listen = {
+        slug: 'listen',
+        title: 'Listen',
+        body: '',
+        nav_position: '02',
+        nav_parents: 'none',
+      };
+      const home = {
+        slug: 'home',
+        title: 'Home',
+        body: '',
+        nav_position: '01',
+        nav_parents: 'none',
+      };
+      this.data.pages.push(listen);
+      this.data.pages.push(home);
+    },
   },
   created: function () {
     // build routes and pass in data
     if (document.getElementById('djangoData')) {
-      this.data = JSON.parse(document.getElementById('djangoData').textContent)
+      this.data = JSON.parse(
+        document.getElementById('djangoData').textContent,
+      );
     }
-    this.create_default_pages()
-    this.generate_page_routes()
-    this.$router.push(`/${this.data.slug}`).catch(err => {
+    this.create_default_pages();
+    this.generate_page_routes();
+    this.$router.push(`/${this.data.slug}`).catch((err) => {
       if (err.name !== 'NavigationDuplicated') {
         throw err;
       }
     });
-    console.log(this.pages)
-  }
-}
-
+    console.log(this.pages);
+  },
+};
 </script>
 
 <style lang="scss">
 @font-face {
-  font-family: "Russian";
-  src: url('../../../assets/fonts/Russian.woff') format('woff'), url('../../../assets/fonts/Russian.woff2') format('woff2');
+  font-family: 'Russian';
+  src: url('../../../assets/fonts/Russian.woff') format('woff'),
+    url('../../../assets/fonts/Russian.woff2') format('woff2');
   font-weight: normal;
   font-style: normal;
   font-display: block;
 }
-@import "@/scss/_common.scss";
+@import '@/scss/_common.scss';
 @include base-style;
 @include link-style;
 @include nav-style;
-
 </style>
 
 <style scoped lang="scss">
-@import "@/scss/_variables.scss";
+@import '@/scss/_variables.scss';
 
 .container {
   display: flex;
@@ -177,20 +182,18 @@ export default {
 .footer {
   height: 20px;
   display: block;
-  position:fixed;
+  position: fixed;
   bottom: 0px;
   padding-bottom: 3px;
   width: 100%;
 }
 
-@media (min-width: map-get($breakpoints, "medium")) {
+@media (min-width: map-get($breakpoints, 'medium')) {
   .main__padding {
     flex: 1;
   }
   .nav {
     height: 20px;
   }
-
 }
-
 </style>
